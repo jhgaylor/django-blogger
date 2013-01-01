7bcd45292effa41ae89cf50b59484c2a79845a66
Jake Gaylor <jhgaylor@gmail.com>
2013-01-01 06:21:52 -0600

    fixed relationships

--- SimpleBlog/Blogger/models.py
+++ SimpleBlog/Blogger/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=200)
@@ -10,16 +10,16 @@ class Tag(models.Model):
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
-
+    user = models.ForeignKey(User)
     def __unicode__(self):
         return self.name
 
 class Post(models.Model):
-    author = models.ForeignKey(Author)
+    author = models.ForeignKey(Author, blank=True)
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
     
     def __unicode__(self):
         return self.title
