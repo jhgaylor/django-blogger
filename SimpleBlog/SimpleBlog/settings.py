052168b39c41755d2815e177b33cd098b9bcc43a
Jake Gaylor <jhgaylor@gmail.com>
2013-01-01 10:47:12 -0600

    added custom context processors

--- SimpleBlog/SimpleBlog/settings.py
+++ SimpleBlog/SimpleBlog/settings.py
@@ -94,6 +94,18 @@ TEMPLATE_LOADERS = (
     
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "Blogger.context_processors.blog_info"
+)
+
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
