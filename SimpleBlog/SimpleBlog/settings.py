66dba859609092faa9511e2ea7cad935f60be48e
Jake Gaylor <jhgaylor@gmail.com>
2013-01-01 10:47:21 -0600

    added south

--- SimpleBlog/SimpleBlog/settings.py
+++ SimpleBlog/SimpleBlog/settings.py
@@ -142,6 +142,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'Blogger',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
