27b48427420ca4f83a7ed87610b3f63c246f22e7
Jake Gaylor <jhgaylor@gmail.com>
2013-01-01 10:44:34 -0600

    Customized admin

--- SimpleBlog/Blogger/admin.py
+++ SimpleBlog/Blogger/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from Blogger.models import Tag, Post, Author, Comment
 
+class TagAdmin(admin.ModelAdmin):
+	pass
+
 class PostAdmin(admin.ModelAdmin):
 	exclude = ('author',)
-
+	list_display = ('title', 'author', 'created_at', 'get_tags')
+	list_filter = ['author', 'created_at']
 	#this makes the author of a post the user that created it.
 	#http://stackoverflow.com/questions/753704/manipulating-data-in-djangos-admin-panel-on-save
 	def save_model(self, request, obj, form, change):
@@ -11,7 +15,14 @@ class PostAdmin(admin.ModelAdmin):
 			obj.author = request.user.author_set.all()[0]
 		obj.save()
 
-admin.site.register(Tag)
+
+class AuthorAdmin(admin.ModelAdmin):
+	pass
+
+class CommentAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Post,PostAdmin)
-admin.site.register(Author)
-admin.site.register(Comment)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Comment, CommentAdmin)
