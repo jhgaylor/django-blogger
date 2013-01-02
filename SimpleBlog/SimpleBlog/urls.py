4367b252add279344dc36f5fd17744c62a1fde00
Jake Gaylor <jhgaylor@gmail.com>
2013-01-02 04:01:15 -0600

    fixed url pattern

--- SimpleBlog/SimpleBlog/urls.py
+++ SimpleBlog/SimpleBlog/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     url(r'^(?P<year>\d{4})/$', 'Blogger.views.list',name="yearly_archive"),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'Blogger.views.list', name="monthly_archive"),
     
-    url(r'^(?P<slug>[\w-]+)/$', 'Blogger.views.view_post', name='view_post'),
+    url(r'^post/(?P<slug>[\w-]+)/$', 'Blogger.views.view_post', name='view_post'),
 
     url(r'^posts/list/', 'Blogger.views.list', name='list'),
     url(r'^admin/', include(admin.site.urls)),
