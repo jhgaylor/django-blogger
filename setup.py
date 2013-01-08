dbc3678ce36fa86151403e6731cdff34712eaaa9
Jake Gaylor <jhgaylor@gmail.com>
2013-01-08 02:35:13 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-simple-blog",
+    version = "0.1.8",
+    author = "Jake Gaylor",
+    author_email = "jake@codegur.us",
+    description = "A package to administer a simple blog via Django.",
+    url = "https://github.com/jhgaylor/django-simple-blog/",
+    install_requires = [ln for ln in open('requirements.txt')],
+    packages = find_packages(),
+    #package_data=package_data,
+    include_package_data = True,
+)
