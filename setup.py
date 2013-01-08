eee6c8d23843f41275667f481a7754078f00b584
Jake Gaylor <jhgaylor@gmail.com>
2013-01-08 11:31:12 -0600

    setup and manifest changes

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-blogger",
-    version = "0.1.8",
+    version = "0.2.0.6",
     author = "Jake Gaylor",
     author_email = "jake@codegur.us",
     description = "A package to administer a simple blog via Django.",
     url = "https://github.com/jhgaylor/django-simple-blog/",
     install_requires = [ln for ln in open('requirements.txt')],
-    packages = find_packages(),
+    packages = ['Blogger', 'Blogger.themes', 'Blogger.themes.4col', 'Blogger.themes.3col', 'Blogger.themes.3col'],#find_packages(), 
     #package_data=package_data,
     include_package_data = True,
 )
