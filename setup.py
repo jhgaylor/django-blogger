b0bb69ced6d5e4739b2c140da93f0d31a54559c7
Jake Gaylor <jhgaylor@gmail.com>
2013-01-08 11:46:10 -0600

    fixed themeing

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-blogger",
-    version = "0.2.0.6",
+    version = "0.2.1",
     author = "Jake Gaylor",
     author_email = "jake@codegur.us",
     description = "A package to administer a simple blog via Django.",
     url = "https://github.com/jhgaylor/django-simple-blog/",
     install_requires = [ln for ln in open('requirements.txt')],
-    packages = ['Blogger', 'Blogger.themes', 'Blogger.themes.4col', 'Blogger.themes.3col', 'Blogger.themes.3col'],#find_packages(), 
+    packages = find_packages(), 
     #package_data=package_data,
     include_package_data = True,
 )
