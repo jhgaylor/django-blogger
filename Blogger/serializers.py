b62370f0fffd342a515b9329195fbff371f1f044
Jake Gaylor <jhgaylor@gmail.com>
2013-01-12 16:32:14 -0600

    updated rss feeds

--- Blogger/serializers.py
+++ Blogger/serializers.py
@@ -4,15 +4,26 @@ from Blogger.models import Post, Author
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
 
-
-    author = serializers.HyperlinkedRelatedField(source='author', view_name='author-detail')
-    #tags = serializers.ManyRelatedField(source='tags')
+    author = serializers.HyperlinkedRelatedField(source='author', view_name=
+                                                 'author-detail')
+    tags = serializers.ManyRelatedField(source='tags')
 
     class Meta:
         model = Post
         fields = ('url', 'author', 'title', 'body',
-                  'created_at', 'published',
+                  'created_at', 'published', 'tags',
                   'slug')
+
+    def restore_object(self, attrs, instance=None):
+        if instance is not None:
+            instance.author = attrs['author']
+            instance.title = attrs['title']
+            instance.body = attrs['body']
+            instance.published = attrs['published']
+            #instance.tags = attrs['tags']
+            instance.slug = attrs['slug']
+            return instance
+        return Post(**attrs)
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
