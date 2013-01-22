35d30c261974836201cd637e3b03bc5b643738a3
Jake Gaylor <jhgaylor@gmail.com>
2013-01-21 18:40:16 -0600

    added a test

--- Blogger/tests/context_processors.py
+++ Blogger/tests/context_processors.py
@@ -1,1 +1,10 @@
-#make sure that the dictionary is added to the request
+from django.test import TestCase
+from rest_framework.reverse import reverse
+
+
+class ContextProcessorTest(TestCase):
+
+    #make sure that the dictionary is added to the request
+    def test_context(self):
+        resp = self.client.get(reverse('all_archive'))
+        self.assertTrue('BLOG_TITLE' in resp.context)
